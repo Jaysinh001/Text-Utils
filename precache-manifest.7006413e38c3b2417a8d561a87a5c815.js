062c0f79c999113ebcb71762920d23924c06ca66
Jaysinh001 <workmailofjay@gmail.com>
2022-11-12 22:49:41 +0530

    Updates

---
+++ precache-manifest.7006413e38c3b2417a8d561a87a5c815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70b117d8b32f7963cdf9",
+    "url": "/Text-Utils/static/css/main.90d942e5.chunk.css"
+  },
+  {
+    "revision": "70b117d8b32f7963cdf9",
+    "url": "/Text-Utils/static/js/main.70b117d8.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/Text-Utils/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "5d90e341a3df36e87278",
+    "url": "/Text-Utils/static/js/2.5d90e341.chunk.js"
+  },
+  {
+    "revision": "a5c297ed956dc2bb2863",
+    "url": "/Text-Utils/static/js/runtime~main.a5c297ed.js"
+  },
+  {
+    "revision": "56bf5e21667689050fde24d0a1281410",
+    "url": "/Text-Utils/index.html"
+  }
+];
